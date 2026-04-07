945326bf105c39f451a6b0232c3259ba33a08ee5
Shivpoojan833 <mvmoraishivpujan12b@gmail.com>
2026-04-07 12:00:50 +0530

    Initial commit - 100 days of Code

--- day6.py
+++ day6.py
@@ -20,4 +20,9 @@ list1 = [1,2,3,["apple","kiwi","Lichi"],4]
 print(list1) #We can include data elements of different data types and we can change the list items at any time (mutable)
 
 tuple1=("lion","tiger","sparrow","giraffe")
-print(tuple1)
+print(tuple1)  #Tuple are like list but they are immutable hence they cannot be changed at later in the code
+
+# Mapped Data
+dict1 = {1:"One",2:"Two",3:"Three",4:"Four"}
+print(dict1)  #This is also called the key value pairs of the python as every item is combined with a key.
+#Every thing in python is an object like dictionary, boolean, integer, list etc., . Because python is an onject oriented programming language.
